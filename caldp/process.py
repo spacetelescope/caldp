432498fc569205d9b1e9475fe7794856099c7268
Rob Swaters <rswaters@stsci.edu>
2021-02-22 21:58:43 +0000

    Add runastrodrizzle for ACS and WFC3 singletons

--- caldp/process.py
+++ caldp/process.py
@@ -487,6 +487,9 @@ class InstrumentManager:
         unassoc = self.unassoc_files(files)
         if unassoc:
             self.run(self.stage1, *unassoc)
+            if self.stage2:
+                self.run(self.stage2, *unassoc)
+            return
 
     def output_files(self):
         """Selects files from the current working directory and uploads them
