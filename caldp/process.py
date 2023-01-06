5b5e88132f5d34477264d97b19a65674e24c0ca1
Brian Hayden <5826711+bhayden53@users.noreply.github.com>
2023-01-06 13:25:08 -0500

    fix bug in extractall call

--- caldp/process.py
+++ caldp/process.py
@@ -86,7 +86,7 @@ def safe_extractall(tar, path=".", members=None, *, numeric_owner=False):
         if not is_within_directory(path, member_path):  # pragma: no cover
             raise Exception("Attempted Path Traversal in Tar File")
 
-    tar.extractall(path, members, numeric_owner)
+    tar.extractall(path, members, numeric_owner=numeric_owner)
 
 
 def get_instrument(ipppssoot):
