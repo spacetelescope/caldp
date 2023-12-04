58201e2c048e5e12cb235d7b52e6c03dd3d60682
Rob Swaters <rswaters@stsci.edu>
2023-12-04 21:11:43 +0000

    update baseline

--- caldp/tests/test_all.py
+++ caldp/tests/test_all.py
@@ -322,7 +322,7 @@ HAP_RESULTS = dict(
 336142080 inputs/hst_9774_01_acs_wfc_f435w_j8ph01g3_drc.fits
 5453 inputs/hst_9774_01_acs_wfc_f435w_j8ph01g5_drc_thumb.jpg
 634177 inputs/hst_9774_01_acs_wfc_f555w_j8ph01_segment-cat.ecsv
-32797 inputs/hst_9774_01_acs_wfc_flc_metawcs_all_ref_cat.ecsv
+32787 inputs/hst_9774_01_acs_wfc_j8p_metawcs_all_ref_cat.ecsv
 7690 inputs/hst_9774_01_acs_wfc_f435w_j8ph01g5_trl.txt
 21024 inputs/hst_9774_01_acs_wfc_f555w_j8ph01_trl.txt
 3509517 inputs/hst_9774_01_acs_wfc_f555w_j8ph01g7_drc.jpg
